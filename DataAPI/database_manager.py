03d5ae14d3315a8a2b399389d018db38499b4008
m1h41l <dimitrovskimihail95@gmail.com>
2015-05-28 20:12:07 +0300

    Updating files

--- DataAPI/database_manager.py
+++ DataAPI/database_manager.py
@@ -1,5 +1,5 @@
 import crawler as cr
-from models import MuscleGroups, DifficultyLevel, Exercises, Base
+from models import MuscleGroups, DifficultyLevel, Exercises, Priority, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
@@ -7,9 +7,61 @@ engine = create_engine('sqlite:///fitness.db')
 Base.metadata.create_all(engine)
 session = Session(bind=engine)
 
-user = session.query(Exercises).filter(Exercises.name == "Vdigane ot lejanka").one()
-#user.muscle_groups.append(MuscleGroups(name='Arms'))
-for name in user.muscle_groups:
-    print(name.name)
+# user.muscle_groups.append(MuscleGroups(name='Arms'))
+"""exerc = session.query(Exercises).filter(Exercises.name == "Vdigane ot lejanka").one()
+for exerc in exerc.priority,exerc.levels, exerc.mg():
 
-session.commit()
+for name in user.muscle_groups:
+    print(name.name)"""
+
+BEGINNER = "beginner"
+INTERMEDIATE = "intermediate"
+ADVANCED = "advanced"
+
+beg_exerc = cr.get_beginners_exercises()
+intm_exerc = cr.get_intermediate_exercises()
+adv_exerc = cr.get_advanced_exercises()
+
+
+def update_exerc(exerc_name, priority, difficulty, muscle_group):
+    db_exerc = session.query(Exercises).filter(
+        Exercises.name == exerc_name).one()
+    db_exerc.priority_groups.append(Priority(priority=int(priority)))
+    db_exerc.levels.append(DifficultyLevel(name=difficulty))
+    db_exerc.muscle_groups.append(MuscleGroups(name=muscle_group))
+
+# beginners
+
+for muscle_group, exercises in beg_exerc.items():
+    for exerc in exercises:
+        print(exerc, "  --  ", muscle_group)
+        priority = input("Enter priority for this exercise:")
+        db_exerc = session.query(Exercises).filter(
+            Exercises.name == exerc).one()
+        if db_exerc is None:
+            session.add(Exercises(name=exerc))
+        update_exerc(exerc, priority, BEGINNER, muscle_group)
+        session.commit()
+
+for muscle_group, exercises in intm_exerc.items():
+    for exerc in exercises:
+        print(exerc, "  --  ", muscle_group)
+        priority = input("Enter priority for this exercise:")
+        db_exerc = session.query(Exercises).filter(
+            Exercises.name == exerc).one()
+        if db_exerc is None:
+            session.add(Exercises(name=exerc))
+        update_exerc(exerc, priority, BEGINNER, muscle_group)
+        session.commit()
+
+for muscle_group, exercises in adv_exerc.items():
+    for exerc in exercises:
+        print(exerc, "  --  ", muscle_group)
+        priority = input("Enter priority for this exercise:")
+        db_exerc = session.query(Exercises).filter(
+            Exercises.name == exerc).one()
+        if db_exerc is None:
+            session.add(Exercises(name=exerc))
+        update_exerc(exerc, priority, BEGINNER, muscle_group)
+        session.commit()
+
--- DataAPI/models.py
+++ DataAPI/models.py
@@ -9,14 +9,13 @@ Base = declarative_base()
 class Exercises(Base):
     __tablename__ = "Exercises"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    priority = Column(Integer)
+    name = Column(String, unique=True)
 
 
 class MuscleGroups(Base):
     __tablename__ = "MuscleGroups"
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String)
     exercise_id = Column(Integer, ForeignKey(Exercises.id))
     muscle_groups = relationship(Exercises, backref="muscle_groups")
 
@@ -24,8 +23,14 @@ class MuscleGroups(Base):
 class DifficultyLevel(Base):
     __tablename__ = "DifficultyLevel"
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String)
     exercise_id = Column(Integer, ForeignKey(Exercises.id))
     levels = relationship(Exercises, backref="levels")
 
 
+class Priority(Base):
+    __tablename__ = "Priority"
+    id = Column(Integer, primary_key=True)
+    priority = Column(Integer)
+    exercise_id = Column(Integer, ForeignKey(Exercises.id))
+    priority_groups = relationship(Exercises, backref="priority_groups")
