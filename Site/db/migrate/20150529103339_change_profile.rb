583bd11b7def505def30d760caec06e1f33d89ef
Kambata <kamen.e.kanev@gmail.com>
2015-05-29 13:50:13 +0300

    add migration for profile

--- Site/db/migrate/20150529103339_change_profile.rb
+++ Site/db/migrate/20150529103339_change_profile.rb
@@ -1,4 +1,6 @@
 class ChangeProfile < ActiveRecord::Migration
   def change
+    add_column :profiles, :first_name, :string
+    add_column :profiles, :last_name, :string
   end
 end
--- Site/db/schema.rb
+++ Site/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527174602) do
+ActiveRecord::Schema.define(version: 20150529103339) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "nickname",   null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20150527174602) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "profiles", ["gender"], name: "index_profiles_on_gender"
